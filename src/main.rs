c22e8f9d6d580ac44d2e1ecfeeaabf3b90e8b0bd
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-05-05 17:36:04 +0200

    [ADD] - parce que c'est plus fancy comme ça

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,62 @@ mod box_pointers;
 mod fnmut_pointers;
 mod rc_pointers;
 mod resume_ex;
-use ansi_term::Colour;
+use ansi_term::{Colour, Style};
 use resume_ex::{exo1, exo2, exo3, exo4, exo5};
 
 // Main de test
 fn main() {
-    println!("{}", Colour::Green.paint("Exercice 1"));
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .underline()
+            .fg(Colour::Green)
+            .paint("Exercice 1")
+    );
     exo1();
 
     println!("");
-    println!("{}", Colour::Green.paint("Exercice 2"));
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .underline()
+            .fg(Colour::Green)
+            .paint("Exercice 2")
+    );
     exo2();
 
     println!("");
-    println!("{}", Colour::Green.paint("Exercice 3"));
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .underline()
+            .fg(Colour::Green)
+            .paint("Exercice 3")
+    );
     exo3();
 
     println!("");
-    println!("{}", Colour::Green.paint("Exercice 4"));
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .underline()
+            .fg(Colour::Green)
+            .paint("Exercice 4")
+    );
     exo4();
 
     println!("");
-    println!("{}", Colour::Green.paint("Exercice 5"));
+    println!(
+        "{}",
+        Style::new()
+            .bold()
+            .underline()
+            .fg(Colour::Green)
+            .paint("Exercice 5")
+    );
     exo5();
 }
